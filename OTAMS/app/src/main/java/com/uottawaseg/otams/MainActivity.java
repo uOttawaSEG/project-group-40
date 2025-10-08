7589bab46c6e079b9f539cc5e4b55d7218803584
Anastasia <annie.sadovski@gmail.com>
2025-10-07 21:37:10 -0400

    Minor UI changes

--- OTAMS/app/src/main/java/com/uottawaseg/otams/MainActivity.java
+++ OTAMS/app/src/main/java/com/uottawaseg/otams/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main); // Edit "x" in R.layout.x to test out different pages - Anastasia
+        setContentView(R.layout.welcome); // Edit "x" in R.layout.x to test out different pages - Anastasia
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
